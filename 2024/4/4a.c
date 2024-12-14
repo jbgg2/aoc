67026149d8a8f2f93afeddd708f61deae74725a9
Bosco G. G. <juan.bosco@uca.es>
2024-12-14 18:12:37 +0100

    4a

--- 2024/4/4a.c
+++ 2024/4/4a.c
@@ -11,8 +11,13 @@ typedef struct let {
 	int x;
 	int y;
 
+	int n;
+
 	struct let *next;
 } let_t;
+
+unsigned int get_sum(let_t **a, int xmax, int ymax);
+void get_n(let_t **a, int xmax, int ymax);
 
 void get_input(FILE *f, let_t **list, int *xmax_p, int *ymax_p);
 
@@ -32,6 +37,8 @@ int main(int argc, char *argv[]){
 	int ymax = 0;
 	let_t **a = NULL;
 
+	unsigned int s = 0;
+
 	f = fopen("input", "r");
 	if(f == NULL)
 		goto cleanup;
@@ -44,12 +51,83 @@ int main(int argc, char *argv[]){
 	if(a == NULL)
 		goto cleanup;
 
+	get_n(a, xmax, ymax);
+
+	s = get_sum(a, xmax, ymax);
+	printf("%u\n", s);
+
 cleanup:
 	free(a);
 	free_list(list);
 	if(f)
 		fclose(f);
 	return 0;
+}
+
+unsigned int get_sum(let_t **a, int xmax, int ymax){
+	int i;
+	int j;
+	unsigned int s = 0;
+	for(i=0;i<xmax;i++){
+		for(j=0;j<ymax;j++){
+			if((*a)->n > 0)
+				print_let(*a);
+			s+=(*a)->n;
+			a++;
+		}
+	}
+	return s;
+}
+
+let_t *get_let(let_t **a, int xmax, int ymax, int x, int y){
+	if(x<0 || x>=xmax || y<0 || y>=ymax)
+		return NULL;
+	return a[y*ymax + x];
+}
+
+/*
+index 0: X
+index 1: M
+index 2: A
+index 3: S
+*/
+int get_n_r(let_t **a, int xmax, int ymax, let_t *l, int index){
+	int r;
+	int x;
+	int y;
+
+	if(l == NULL)
+		return 0;
+
+	x = l->x;
+	y = l->y;
+
+	r = 0;
+	if((index == 0 && l->c == 'X') || (index == 1 && l->c == 'M') || (index == 2 && l->c == 'A')){
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x-1, y-1), index+1);
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x, y-1), index+1);
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x+1, y-1), index+1);
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x+1, y), index+1);
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x+1, y+1), index+1);
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x, y+1), index+1);
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x-1, y+1), index+1);
+		r += get_n_r(a, xmax, ymax, get_let(a, xmax, ymax, x-1, y), index+1);
+	}else if(index == 3 && l->c == 'S')
+		return 1;
+	return r;
+}
+
+void get_n(let_t **a, int xmax, int ymax){
+	let_t **pp;
+	int i;
+	int j;
+	pp = a;
+	for(i=0;i<xmax;i++){
+		for(j=0;j<ymax;j++){
+			(*pp)->n = get_n_r(a, xmax, ymax, *pp, 0);
+			pp++;
+		}
+	}
 }
 
 void get_input(FILE *f, let_t **list_p, int *xmax_p, int *ymax_p){
@@ -90,6 +168,7 @@ let_t *create_let(int x, int y, char c, let_t *next){
 	p->x = x;
 	p->y = y;
 	p->c = c;
+	p->n = 0;
 	p->next = next;
 	return p;
 }
@@ -107,7 +186,7 @@ void free_list(let_t *list){
 }
 
 void print_let(let_t *l){
-	printf("(%d,%d) %c\n", l->x, l->y, l->c);
+	printf("(%d,%d) %c; %d\n", l->x, l->y, l->c, l->n);
 }
 
 void print_list(let_t *list){
